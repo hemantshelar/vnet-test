53ad413589d13f8280e1ca2b656dba09b44deb7d
Hemant <hemant.shelar@gmail.com>
2024-09-05 06:30:12 +1000

    Added dataverse code.

---
+++ Controllers/DataVerseController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.PowerPlatform.Dataverse.Client;
+
+namespace WebApplication1.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class DataVerseController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult ConnectToDataVerse()
+		{
+			string url = Environment.GetEnvironmentVariable("URL") ?? "Not found";
+			string clientId = Environment.GetEnvironmentVariable("CLIENT_ID") ?? "";
+			string clientSecret = Environment.GetEnvironmentVariable("SECRET") ?? "";
+			string result = "Success";
+			try
+			{
+				string connectionString = $@"
+AuthType = ClientSecret;
+Url = {url};
+ClientId = {clientId}
+Secret = {clientSecret}";
+				var sc = new ServiceClient(connectionString);
+			}
+			catch (Exception ex)
+			{
+				result = ex.Message;
+			}
+			return Ok(result);
+		}
+	}
+}
